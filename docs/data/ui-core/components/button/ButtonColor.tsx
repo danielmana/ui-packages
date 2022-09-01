699d2380da67cdae5348e68528d1cba7d4165bb9
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2022-09-01 23:23:33 +0200

    [docs][ui-core][button] Initial docs for ui-core/Button

---
+++ docs/data/ui-core/components/button/ButtonColor.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { Button, createTheme, ThemeProvider } from '@danielmana/ui-core';
+import Stack from '@mui/material/Stack';
+
+export default function ButtonColor() {
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Stack direction="row" spacing={2}>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="tertiary">Tertiary</Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+      </Stack>
+    </ThemeProvider>
+  );
+}
