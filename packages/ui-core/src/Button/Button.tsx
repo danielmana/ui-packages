8e8dacf246c813d7589902abcbbe32ebe69f3a5a
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2022-09-05 15:00:17 +0200

    [ui-core][button] Improve props generation

--- packages/ui-core/src/Button/Button.tsx
+++ packages/ui-core/src/Button/Button.tsx
@@ -1,84 +1,8 @@
-import React, { Ref } from 'react';
-
+import * as React from 'react';
+import PropTypes from 'prop-types';
 import MuiButton from '@mui/material/Button';
-import { ButtonBaseProps } from '@mui/material/ButtonBase';
-import { Theme } from '@mui/material/styles';
-import { SxProps } from '@mui/system';
 
-export interface ButtonProps extends Omit<ButtonBaseProps, 'classes' | 'className'> {
-  /**
-   * The content of the component.
-   */
-  children?: React.ReactNode;
-  /**
-   * The color of the component.
-   * It supports both default and custom theme colors, which can be added as shown in the
-   * [palette customization guide](https://mui.com/material-ui/customization/palette/#adding-new-colors).
-   * @default 'primary'
-   */
-  color?:
-    | 'inherit'
-    | 'primary'
-    | 'secondary'
-    | 'tertiary'
-    | 'success'
-    | 'error'
-    | 'info'
-    | 'warning';
-  /**
-   * If `true`, the component is disabled.
-   * @default false
-   */
-  disabled?: boolean;
-  /**
-   * If `true`, no elevation is used.
-   * @default false
-   */
-  disableElevation?: boolean;
-  /**
-   * If `true`, the  keyboard focus ripple is disabled.
-   * @default false
-   */
-  disableFocusRipple?: boolean;
-  /**
-   * Element placed after the children.
-   */
-  endIcon?: React.ReactNode;
-  /**
-   * If `true`, the button will take up the full width of its container.
-   * @default false
-   */
-  fullWidth?: boolean;
-  /**
-   * The URL to link to when the button is clicked.
-   * If defined, an `a` element will be used as the root node.
-   */
-  href?: string;
-  /**
-   * The size of the component.
-   * `small` is equivalent to the dense button styling.
-   * @default 'medium'
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Element placed before the children.
-   */
-  startIcon?: React.ReactNode;
-  /**
-   * The system prop that allows defining system overrides as well as additional CSS styles.
-   */
-  sx?: SxProps<Theme>;
-  /**
-   * The variant to use.
-   * @default 'text'
-   */
-  variant?: 'text' | 'outlined' | 'contained';
-  /**
-   * The component used for the root node.
-   * Either a string to use a HTML element or a component.
-   */
-  component?: React.ElementType;
-}
+import { ExtendButton, ButtonTypeMap } from './ButtonProps';
 /**
  *
  * Demos:
@@ -90,8 +14,95 @@ export interface ButtonProps extends Omit<ButtonBaseProps, 'classes' | 'classNam
  * - [Button API](https://mui.com/ui-core/api/button/)
  * - inherits [ButtonBase API](https://mui.com/material-ui/api/button-base/)
  */
-const Button: React.FC<ButtonProps> = React.forwardRef((props: ButtonProps, ref: Ref<any>) => {
+const Button = React.forwardRef(function Button(props, ref) {
   return <MuiButton {...props} ref={ref} />;
-});
+}) as ExtendButton<ButtonTypeMap>;
+
+Button.propTypes /* remove-proptypes */ = {
+  // ----------------------------- Warning --------------------------------
+  // | These PropTypes are generated from the TypeScript type definitions |
+  // |     To update them edit TypeScript types and run "yarn proptypes"  |
+  // ----------------------------------------------------------------------
+  /**
+   * The content of the component.
+   */
+  children: PropTypes.node,
+  /**
+   * The color of the component.
+   * It supports both default and custom theme colors, which can be added as shown in the
+   * [palette customization guide](https://mui.com/material-ui/customization/palette/#adding-new-colors).
+   * @default 'primary'
+   */
+  color: PropTypes.oneOf([
+    'error',
+    'info',
+    'inherit',
+    'primary',
+    'secondary',
+    'success',
+    'tertiary',
+    'warning',
+  ]),
+  /**
+   * If `true`, the component is disabled.
+   * @default false
+   */
+  disabled: PropTypes.bool,
+  /**
+   * If `true`, no elevation is used.
+   * @default false
+   */
+  disableElevation: PropTypes.bool,
+  /**
+   * If `true`, the  keyboard focus ripple is disabled.
+   * @default false
+   */
+  disableFocusRipple: PropTypes.bool,
+  /**
+   * If `true`, the ripple effect is disabled.
+   *
+   * ⚠️ Without a ripple there is no styling for :focus-visible by default. Be sure
+   * to highlight the element by applying separate styles with the `.Mui-focusVisible` class.
+   * @default false
+   */
+  disableRipple: PropTypes.bool,
+  /**
+   * Element placed after the children.
+   */
+  endIcon: PropTypes.node,
+  /**
+   * If `true`, the button will take up the full width of its container.
+   * @default false
+   */
+  fullWidth: PropTypes.bool,
+  /**
+   * The URL to link to when the button is clicked.
+   * If defined, an `a` element will be used as the root node.
+   */
+  href: PropTypes.string,
+  /**
+   * The size of the component.
+   * `small` is equivalent to the dense button styling.
+   * @default 'medium'
+   */
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  /**
+   * Element placed before the children.
+   */
+  startIcon: PropTypes.node,
+  /**
+   * The system prop that allows defining system overrides as well as additional CSS styles.
+   */
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool])),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+  /**
+   * The variant to use.
+   * @default 'text'
+   */
+  variant: PropTypes.oneOf(['contained', 'outlined', 'text']),
+} as any;
 
 export default Button;
---
+++ packages/ui-core/src/Button/ButtonProps.ts
@@ -0,0 +1,91 @@
+import React from 'react';
+import { SxProps, Theme } from '@mui/material/styles';
+import { ExtendButtonBaseTypeMap } from '@mui/material/ButtonBase';
+import {
+  OverridableComponent,
+  OverridableTypeMap,
+  OverrideProps,
+} from '@mui/material/OverridableComponent';
+
+export interface ButtonTypeMap<P = {}, D extends React.ElementType = 'button'> {
+  props: P & {
+    /**
+     * The content of the component.
+     */
+    children?: React.ReactNode;
+    /**
+     * The color of the component.
+     * It supports both default and custom theme colors, which can be added as shown in the
+     * [palette customization guide](https://mui.com/material-ui/customization/palette/#adding-new-colors).
+     * @default 'primary'
+     */
+    color?:
+      | 'inherit'
+      | 'primary'
+      | 'secondary'
+      | 'tertiary'
+      | 'success'
+      | 'error'
+      | 'info'
+      | 'warning';
+    /**
+     * If `true`, the component is disabled.
+     * @default false
+     */
+    disabled?: boolean;
+    /**
+     * If `true`, no elevation is used.
+     * @default false
+     */
+    disableElevation?: boolean;
+    /**
+     * If `true`, the  keyboard focus ripple is disabled.
+     * @default false
+     */
+    disableFocusRipple?: boolean;
+    /**
+     * Element placed after the children.
+     */
+    endIcon?: React.ReactNode;
+    /**
+     * If `true`, the button will take up the full width of its container.
+     * @default false
+     */
+    fullWidth?: boolean;
+    /**
+     * The URL to link to when the button is clicked.
+     * If defined, an `a` element will be used as the root node.
+     */
+    href?: string;
+    /**
+     * The size of the component.
+     * `small` is equivalent to the dense button styling.
+     * @default 'medium'
+     */
+    size?: 'small' | 'medium' | 'large';
+    /**
+     * Element placed before the children.
+     */
+    startIcon?: React.ReactNode;
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
+    sx?: SxProps<Theme>;
+    /**
+     * The variant to use.
+     * @default 'text'
+     */
+    variant?: 'text' | 'outlined' | 'contained';
+  };
+  defaultComponent: D;
+}
+
+export type ButtonProps<
+  D extends React.ElementType = ButtonTypeMap['defaultComponent'],
+  P = {},
+> = OverrideProps<ButtonTypeMap<P, D>, D>;
+
+export type ExtendButton<M extends OverridableTypeMap> = ((
+  props: { href: string } & OverrideProps<ExtendButtonBaseTypeMap<M>, 'a'>,
+) => JSX.Element) &
+  OverridableComponent<ExtendButtonBaseTypeMap<M>> & { propTypes?: any };
