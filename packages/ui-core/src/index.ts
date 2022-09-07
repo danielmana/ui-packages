9ee488e7865628583a7b2f5d611cf671f1f57ec0
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2022-09-07 13:45:04 +0200

    [ui-core] Omit unused exports

--- packages/ui-core/src/index.ts
+++ packages/ui-core/src/index.ts
@@ -1,11 +1,3 @@
-/* MUI */
-import * as colors from '@mui/material/colors';
-
-export { colors };
-
-export * from '@mui/material/utils';
-
-/* MUI wrapped */
 export * from './styles';
 
 export { default as Button } from './Button';
