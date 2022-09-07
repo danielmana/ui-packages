4e7661636002d712a5d57b5067322e7e1ca14760
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2022-09-07 14:41:27 +0200

    [ui-core][styles] Extend components

--- packages/ui-core/src/styles/createTheme.ts
+++ packages/ui-core/src/styles/createTheme.ts
@@ -1,9 +1,6 @@
 import { teal } from '@mui/material/colors';
 import {
-  adaptV4Theme as muiAdaptV4Theme,
-  createTheme as muiCreateTheme,
-  DeprecatedThemeOptions,
-  ThemeOptions,
+    adaptV4Theme, createTheme as muiCreateTheme, DeprecatedThemeOptions, ThemeOptions
 } from '@mui/material/styles';
 
 const { palette } = muiCreateTheme();
@@ -12,15 +9,22 @@ const { augmentColor } = palette;
 export const createColor = (mainColor: string) => augmentColor({ color: { main: mainColor } });
 
 /**
- * Generate a theme base on the V4 theme options received.
+ * Generate a sanitized theme based on the theme options received.
  * @param options Takes an incomplete theme object and adds the missing parts.
  * @returns A complete, ready-to-use theme object.
  */
-export const adaptV4Theme = (options: ThemeOptions | DeprecatedThemeOptions) => {
-  const result = muiAdaptV4Theme(options);
+const sanitizeThemeOptions = (options: Partial<ThemeOptions> | DeprecatedThemeOptions) => {
+  const result = adaptV4Theme(options);
 
   // Ensure additional colors exist
   result.palette.tertiary ||= createColor(teal.A700);
+
+  // Extend components in case of using a V5 theme
+  const components = (options as Partial<ThemeOptions>).components;
+  result.components = {
+    ...result.components,
+    ...components,
+  };
 
   // Remove legacy components
   delete result.components?.MuiStepConnector;
@@ -35,7 +39,7 @@ export const adaptV4Theme = (options: ThemeOptions | DeprecatedThemeOptions) => 
  * @param args Deep merge the arguments with the about to be returned theme.
  * @returns A complete, ready-to-use theme object for MUI v5
  */
-const createTheme = (options?: ThemeOptions | DeprecatedThemeOptions, ...args: object[]) =>
-  muiCreateTheme(adaptV4Theme(options || {}), args);
+const createTheme = (options?: Partial<ThemeOptions> | DeprecatedThemeOptions, ...args: object[]) =>
+  muiCreateTheme(sanitizeThemeOptions(options || {}), args);
 
 export default createTheme;
