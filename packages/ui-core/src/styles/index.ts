4c2cb9eba5456c8ac4a89f29aeb8c5c44a9f28fd
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2022-09-07 13:32:52 +0200

    [ui-core] Omit unused exports

--- packages/ui-core/src/styles/index.ts
+++ packages/ui-core/src/styles/index.ts
@@ -1,31 +1,5 @@
 /* MUI */
-export { default as adaptV4Theme } from '@mui/material/styles/adaptV4Theme';
-export {
-  hexToRgb,
-  rgbToHex,
-  hslToRgb,
-  decomposeColor,
-  recomposeColor,
-  getContrastRatio,
-  getLuminance,
-  emphasize,
-  alpha,
-  darken,
-  lighten,
-  css,
-  keyframes,
-  experimental_sx,
-} from '@mui/system';
-export { default as createStyles } from '@mui/material/styles/createStyles';
-export { default as responsiveFontSizes } from '@mui/material/styles/responsiveFontSizes';
-export { duration, easing } from '@mui/material/styles/createTransitions';
-export { default as useTheme } from '@mui/material/styles/useTheme';
-export { default as useThemeProps } from '@mui/material/styles/useThemeProps';
-export { default as styled } from '@mui/material/styles/styled';
 export { default as ThemeProvider } from '@mui/material/styles/ThemeProvider';
-export { StyledEngineProvider } from '@mui/system';
-
-export * from '@mui/material/styles/CssVarsProvider';
 
 /* MUI wrapped */
 export { default as createTheme } from './createTheme';
