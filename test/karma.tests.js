5fac90f3fca8f8d4297d3e6f94694700063b737a
Daniel Mañá Guixà <daniel81bcn@gmail.com>
2022-09-17 00:00:54 +0200

    [cleanup] Configure karma tests + prettier

--- test/karma.tests.js
+++ test/karma.tests.js
@@ -1,46 +1,12 @@
 import './utils/init';
 import './utils/setupKarma';
 
-const materialIntegrationContext = require.context(
-  '../packages/mui-material/test/integration',
+const uiCoreContext = require.context('../packages/ui-core/src/', true, /\.test\.(js|ts|tsx)$/);
+uiCoreContext.keys().forEach(uiCoreContext);
+
+const uiComponentsContext = require.context(
+  '../packages/ui-components/src/',
   true,
   /\.test\.(js|ts|tsx)$/,
 );
-materialIntegrationContext.keys().forEach(materialIntegrationContext);
-
-const baseIntegrationContext = require.context(
-  '../packages/mui-base/test/integration',
-  true,
-  /\.test\.(js|ts|tsx)$/,
-);
-baseIntegrationContext.keys().forEach(baseIntegrationContext);
-
-const materialUnitContext = require.context(
-  '../packages/mui-material/src/',
-  true,
-  /\.test\.(js|ts|tsx)$/,
-);
-materialUnitContext.keys().forEach(materialUnitContext);
-
-const styledEngineContext = require.context(
-  '../packages/mui-styled-engine/src/',
-  true,
-  /\.test\.(js|ts|tsx)$/,
-);
-styledEngineContext.keys().forEach(styledEngineContext);
-
-const styledEngineSCContext = require.context(
-  '../packages/mui-styled-engine-sc/src/',
-  true,
-  /\.test\.(js|ts|tsx)$/,
-);
-styledEngineSCContext.keys().forEach(styledEngineSCContext);
-
-const systemContext = require.context('../packages/mui-system/src/', true, /\.test\.(js|ts|tsx)$/);
-systemContext.keys().forEach(systemContext);
-
-const baseUnitContext = require.context('../packages/mui-base/src/', true, /\.test\.(js|ts|tsx)$/);
-baseUnitContext.keys().forEach(baseUnitContext);
-
-const utilsContext = require.context('../packages/mui-utils/src/', true, /\.test\.(js|ts|tsx)$/);
-utilsContext.keys().forEach(utilsContext);
+uiComponentsContext.keys().forEach(uiComponentsContext);
